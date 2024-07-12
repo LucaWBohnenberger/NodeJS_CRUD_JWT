ae5a84524c2c2b48b7e49affc7d0e4eedb2f0a16
Luca <lucabohneberger@gmail.com>
2024-07-12 17:56:11 -0300

    token and login, bug in route name '/report', change to '/opa'

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+dotenv.config();
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
 
-app.get('/users', async (req, res) => {
+//Rota de authenticação
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
+  if(!email || !password) {
+    return res.status(422).json({ error: 'Email or password is missing' });
+  }
   
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  }); 
+
+  if(!userExists) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, userExists.password);
+
+  if(!isPasswordCorrect) {
+    return res.status(422).json({ error: 'Password is incorrect' });
+  }
+
+  const secret = process.env.SECRET;
+  const token = jwt.sign({ id: userExists.id }, secret)
+
+  res.status(200).json({msg: 'Login successfully', token});
+
+})
+
+
+//CRUD users
+app.get('/users', async (req, res) => {
   const users = await prisma.user.findMany();
 
+  res.status(200).json(users);
+});
+
+app.get('/users/:id', async (req, res) => {
+  const users = await prisma.user.findUnique({
+    where: {
+      id: req.params.id,
+    },
+  });
 
   res.status(200).json(users);
 });
@@ -19,13 +63,37 @@ app.get('/users', async (req, res) => {
 
 app.post('/users', async (req, res) => {
 
+  const { email, password , level, name} = req.body;
+
+  if(!email || !password || !name) {
+    return res.status(422).json({ error: 'Email, password or name is missing' });
+  }
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  }); 
+
+  if(userExists) {
+    return res.status(422).json({ error: 'Email alredy registered, please use another email' });
+  }
+
+  //Modiify password to hash
+  const salt = await bcrypt.genSalt(11);
+  const hash = await bcrypt.hash(password, salt);
+
+  //Create user
   await prisma.user.create({
     data: {
-      name: req.body.name,
-      email: req.body.email,
-      age: req.body.age,
+      name,
+      level,
+      email,
+      password: hash,
     },
-  });
+  }
+
+  );
 
   res.status(201).json(req.body);
 });
@@ -33,14 +101,21 @@ app.post('/users', async (req, res) => {
 
 app.put('/users/:id', async (req, res) => {
 
+  const { email, password , level, name} = req.body;
+
+  //Modiify password to hash
+  const salt = await bcrypt.genSalt(11);
+  const hash = await bcrypt.hash(password, salt);
+
   await prisma.user.update({
     where:{
       id: req.params.id,
     },
     data: {
-      name: req.body.name,
-      email: req.body.email,
-      age: req.body.age,
+      name,
+      email,
+      level,
+      password: hash,
     },
   });
 
@@ -59,7 +134,41 @@ app.delete('/users/:id', async (req, res) => {
     res.status(204).send();
 });
 
+//Private route
+app.get('/opa', verifyJWT, async (req, res) => {
 
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.body.id,
+    },
+  });
+
+  if(user.level < 4) {
+    res.status(401).json({ msg:'Access denied'});
+  }
+
+  res.status(201).json({ msg:'Access granted'});
+
+
+})
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if(!token) {
+    res.status(401).json({ msg:'Access denied'});
+  }
+
+  try{
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+
+  } catch(error) {
+    res.status(401).json({ msg:'Invalid token'});
+  }
+}
 
 
 
